bf5319c63019888e9e8a838ccbc9a47b0ec6e00e
Celine Chan <cctinlone@gmail.com>
2022-09-29 12:26:17 +0200

    Test js array and display in html

---
+++ javascript/proj1main.js
@@ -0,0 +1,48 @@
+let baseInfo = [
+  {
+    continent: "Asia",
+    country: "South Korea",
+    city: "Seoul",
+    dateOftravel: "August 2015",
+  },
+  {
+    continent: "Asia",
+    country: "Vietnam",
+    city: "Hanoi",
+    dateOftravel: "September 2014",
+  },
+  {
+    continent: "Europe",
+    country: "Spain",
+    city: "Barcelona",
+    dateOftravel: "July 2013",
+  },
+  {
+    continent: "Europe",
+    country: "Portugal",
+    city: "Funchal",
+    dateOftravel: "October 2019",
+  },
+  {
+    continent: "North America",
+    country: "Mexico",
+    city: "Mexico City",
+    dateOftravel: "Feburary 2017",
+  },
+];
+const placeName = document.querySelector("#placeName");
+
+function listInfo(info) {
+  let outcome = `<p> ${info.continent} in ${info.country}! The city is ${info.city}, ${info.dateOftravel}! `;
+  return outcome;
+}
+
+function fillPlaces(travelInfo) {
+  for (let i = 0; i < baseInfo.length; i++) {
+    let output = listInfo(baseInfo[i]);
+    console.log(baseInfo[i]);
+
+    placeName.innerHTML += output;
+  }
+}
+fillPlaces();
