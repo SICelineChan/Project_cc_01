913efa8f8c727d204cfdeb0c5a9691467f3748b7
Celine Chan <cctinlone@gmail.com>
2022-10-01 18:48:09 +0200

    changes to weatherTest js to include city name

--- javascript/weatherTEST.js
+++ javascript/weatherTEST.js
@@ -4,16 +4,19 @@ function cityForecast(somePlace) {
     console.log(somePlace[i].weather[0].description);
     console.log(somePlace[i].dt_txt);
     const wetterForecast = i + " : " + somePlace[i].weather[0].description;
-    weather.innerHTML += `<p> This is the weather forecast ${wetterForecast} and
+    weather.innerHTML += `<p> This is the weather forecast for a City and these are the ${wetterForecast}
     at these time and dates ${somePlace[i].dt_txt}  </p>`;
   }
 }
-
+const forecastForCity = document.querySelector("#forecastForCity");
 fetch(
   "https://api.openweathermap.org/data/2.5/forecast?lat=38.7167&lon=-9.1333&appid=fe9b9d62a21b37d39baab5ed769d8339&units=metric"
 ).then(function (response) {
   response.json().then(function (forecast) {
     console.log(forecast);
+    const forecastForCity = document.querySelector("#forecastForCity");
+    forecastForCity.innerHTML += `This is the weather forecast for ${forecast.city.name} `;
+    console.log(forecast.city.name);
     console.log(forecast.list.length);
     let predictList = forecast.list;
 
